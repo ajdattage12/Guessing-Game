fed1af791dc503ba599d8d3540f9d7cd82d35aef
Ariell Dattage <ariell.dattage@Ariells-MacBook-Pro.local>
2021-12-07 11:07:24 -0700

    added counter

--- game.py
+++ game.py
@@ -1,20 +1,25 @@
 import random
+import math
 
 user_name = input("Hello there! What's your name? ")
-response = input (f"Do you want to play a game {user_name}? ")
-
-game_start = input(f"I am thinking of a number between 1 and 100. Ready? ")
+print(f"{user_name}, I am thinking of a number between 1 and 100. ")
 
 num = random.randint(1, 100)
 guess = None
+guessesTaken = 0
 
 while guess != num:
     guess = input("Try to guess the number! ")
     guess = int(guess)
+    guessesTaken = guessesTaken + 1
 
     if guess == num:
-        print(f"Congratulations {user_name}! You won!")
+        print(f"Congratulations {user_name}! You won in {guessesTaken} guesses!")
         break
+    elif guess > 100:
+        print("Please pick a number between 1 - 100.")
+    elif guess < 1:
+        print("Please pick an number between 1 - 100.")
     elif guess < num:
         print("Your guess is too low, try again!")
     elif guess > num:
